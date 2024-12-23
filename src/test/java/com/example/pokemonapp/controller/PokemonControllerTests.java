cad5404b6928d64ff1a7e0831e68cb90ffbebeb3
Joshua <joshua_permaul@yahoo.com>
2024-12-22 23:57:43 -0600

    Updating Test Files

--- src/test/java/com/example/pokemonapp/controller/PokemonControllerTests.java
+++ src/test/java/com/example/pokemonapp/controller/PokemonControllerTests.java
@@ -1,5 +1,25 @@
 package com.example.pokemonapp.controller;
 
-public class PokemonControllerTests {
+import com.example.pokemonapp.model.Pokemon;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.bind.annotation.RequestBody;
 
+public class PokemonControllerTests {
+//
+//    @Autowired
+//    PokemonController pokemonController;
+//
+//    MockMvc mockMvc;
+//    @Test
+//    void addPokemonTest(String Authorization, @RequestBody Pokemon pokemon) {
+//        //Mocking Controller
+//        controllerToTest = mock(HelloWorld.class);
+//
+//        this.mockMvc.perform(get("/addPokemon")
+//                        .accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
+//                .andExpect(status().isOk())
+//                .andExpect(content().mimeType(MediaType.APPLICATION_JSON));
+//    }
 }
